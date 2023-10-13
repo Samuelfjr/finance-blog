ef6fd851c9f802714ce636ff828b1fd46eba7155
Samuelfjr <samucaftww@gmail.com>
2023-10-13 20:42:22 -0300

    feat: Estrutura Inicial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Roboto, Sora } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '500', ],
+  subsets: ['latin'],
+  variable: '--font-roboto'
+})
+
+const sora = Sora({
+  weight: ['600', '700'],
+  subsets: ['latin'],
+  variable: '--font-sora'
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return ( 
+    <main>
+      <Component {...pageProps} />
+    </main>
+  )
 }
