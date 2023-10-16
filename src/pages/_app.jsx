36ef0bd144e4f73c34fa9c08dc4a93faa796084c
Samuelfjr <samucaftww@gmail.com>
2023-10-16 18:35:46 -0300

    feat: Header Parcial

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,9 @@
-import type { AppProps } from 'next/app'
 import { roboto, sora } from '../styles/fonts'
 
 import '../styles/globals.scss'
 
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return ( 
     <main className={ roboto.className }>
       <Component {...pageProps} />
