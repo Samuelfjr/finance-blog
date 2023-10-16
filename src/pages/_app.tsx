c9f762569dd89aa741979a8afca28e56e29c1e7c
Samuelfjr <samucaftww@gmail.com>
2023-10-16 12:52:15 -0300

    feat: Variáveis Mixins

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import { roboto, sora } from '../styles/fonts'
 
+import '../styles/globals.scss'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return ( 
