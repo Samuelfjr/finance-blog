3a2e2664bb1f6bc5458f173d2c7589a08f38f160
Samuelfjr <samucaftww@gmail.com>
2023-10-17 09:18:37 -0300

    feat: Header Parcial

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,37 @@
 import { roboto } from '@/styles/fonts';
+import Image from 'next/image'
+import Link from 'next/link';
+
 import styles from './Header.module.scss';
 
 const Header = () => {
   return (
     <section className={styles.header}>
-      <span>Novos artigos todas as terças!</span>
+      <div className={styles.containerHeader}>
+
+        <Image 
+          src="/logo.svg"
+          width={360}
+          height={26}
+          alt='logo do Blog Rubens Machado'
+        />
+
+        <ul className={styles.navBar}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/">Artigos</Link>
+          </li>
+          <li>
+            <Link href="/">Contato</Link>
+          </li>
+           <li>
+            <Link href="/">Materiais de Apoio</Link>
+          </li>
+        </ul>      
+ 
+      </div>
     </section>
   );
 };
