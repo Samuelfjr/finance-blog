ccb92b4f0ad8811e0c6ccd1d07c108d7fffbd8eb
Samuelfjr <samucaftww@gmail.com>
2023-10-17 18:53:27 -0300

    feat: Seção Material Apoio

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,67 @@
+import { sora, roboto } from '@/styles/fonts';
+import styles from '../styles/Home.module.scss';
+
+import Image from 'next/image'
+import Link from 'next/link';
+
 export default function Home() {
   return (
-      <h1>Teste</h1>
+    <main className={styles.page}>
+      <div className={styles.containerPage}>
+      <section className={styles.supportMaterial}>
+        <div className={styles.title}>
+          <h1 className={sora.className}>
+          Explore Nosso Material de Apoio e Transforme Suas Finanças Hoje Mesmo
+          </h1>
+        </div>
+
+        <div className={styles.backingCardContainer}>
+          <div className={styles.card}>
+            <Image 
+              src="/spreadsheet.jpg"
+              width={367}
+              height={200}
+              alt='planilha'
+            />
+
+            <div className={styles.partCard}>
+              <h3 className={sora.className}>Calculadora Primeiro Milhão</h3>
+
+              <p className={roboto.className}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</p>
+
+              <div className={styles.buttonContent}>
+                <Link className={roboto.className} href='/'>Acessar Conteúdo</Link>
+              </div>
+            </div>
+
+          </div>
+
+          <div className={styles.card}>
+            <Image 
+              src="/ebook.jpg"
+              width={367}
+              height={200}
+              alt='ebook'
+            />
+
+            <div className={styles.partCard}>
+              <h3 className={sora.className}>Ebook Bolsa de Valores</h3>
+
+              <p className={roboto.className}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</p>
+
+              <div className={styles.buttonContent}>
+                <Link className={roboto.className} href='/'>Acessar Conteúdo</Link>
+              </div>
+            </div>
+
+          </div>
+
+         
+        </div>
+        
+      </section>
+
+      </div>
+    </main>
     )
 }
