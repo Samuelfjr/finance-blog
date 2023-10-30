e265fd101cb2bd7a29f686c10eb5de26a703ea0d
Samuelfjr <samucaftww@gmail.com>
2023-10-30 15:57:29 -0300

    refactor: Logo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,38 +1,37 @@
-import { roboto, sora } from '@/styles/fonts';
-import Image from 'next/image'
+import { roboto, sora } from "@/styles/fonts";
+import Image from "next/image";
 
-import styles from './Hero.module.scss';
+import styles from "./Hero.module.scss";
 
 const Hero = () => {
   return (
-   <section className={styles.hero}>
-    <div className={styles.containerHero}>
-      <Image 
-          alt='imagem de prédios'
-          src="/hero.jpg"
-          fill
-          sizes="100vw"  
-      />
-    </div>
-
-    <div className={styles.legend}>
-      <div className={styles.containerLegend}>
-        <h1 className={sora.className}>
-          Desvendando o Mundo das Finanças
-        </h1>
-        <h4>Rubens Machado</h4>
+    <section className={styles.hero}>
+      <div className={styles.containerHero}>
+        <Image alt="imagem de prédios" src="/hero.jpg" fill sizes="100vw" />
       </div>
+
+      <div className={styles.legend}>
+        <div className={styles.containerLegend}>
+          <h1 className={sora.className}>Desvendando o Mundo das Finanças</h1>
+          <h4 className={sora.className}>Rubens Machado</h4>
+        </div>
         <div className={styles.containerSearchBar}>
-          <input className={styles.searchBar} id='search-bar' type="text" name='searchbar' placeholder='Buscar' />
-          <Image 
-            src='/search.svg'
-            alt='lupa de pesquisa'
+          <input
+            className={styles.searchBar}
+            id="search-bar"
+            type="text"
+            name="searchbar"
+            placeholder="Buscar"
+          />
+          <Image
+            src="/search.svg"
+            alt="lupa de pesquisa"
             width={18}
             height={18}
           />
         </div>
       </div>
-   </section>
+    </section>
   );
 };
 
