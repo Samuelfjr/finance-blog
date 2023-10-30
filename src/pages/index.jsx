21a3d431680538f9b52895c9564284652c4b2ec4
Samuelfjr <samucaftww@gmail.com>
2023-10-30 14:53:36 -0300

    refactor: Home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import { sora, roboto } from "@/styles/fonts";
 import styles from "../styles/Home.module.scss";
+import CardMaterial from "../components/CardMaterial/CardMaterial";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -17,52 +18,12 @@ export default function Home() {
           </div>
 
           <div className={styles.backingCardContainer}>
-            <div className={styles.card}>
-              <Image
-                src="/spreadsheet.jpg"
-                width={367}
-                height={200}
-                alt="planilha"
-              />
-
-              <div className={styles.partCard}>
-                <h3 className={sora.className}>Calculadora Primeiro Milhão</h3>
-
-                <p className={roboto.className}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut
-                </p>
-
-                <div className={styles.buttonContent}>
-                  <Link className={roboto.className} href="/">
-                    Acessar Conteúdo
-                  </Link>
-                </div>
-              </div>
-            </div>
-
-            <div className={styles.card}>
-              <Image src="/ebook.jpg" width={367} height={200} alt="ebook" />
-
-              <div className={styles.partCard}>
-                <h3 className={sora.className}>Ebook Bolsa de Valores</h3>
-
-                <p className={roboto.className}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut
-                </p>
-
-                <div className={styles.buttonContent}>
-                  <Link className={roboto.className} href="/">
-                    Acessar Conteúdo
-                  </Link>
-                </div>
-              </div>
-            </div>
+            <CardMaterial />
+            <CardMaterial />
           </div>
 
           <div className={styles.allSupportMaterials}>
-            <Link className={roboto.className} href="/">
+            <Link className={roboto.className} href="/support-material">
               Ver todos Materias de Apoio
             </Link>
           </div>
