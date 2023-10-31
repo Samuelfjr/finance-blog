6aae99a41a03d07bb5cd60caf679e3bc8101f88c
Samuelfjr <samucaftww@gmail.com>
2023-10-31 20:20:36 -0300

    chore: Apollo Client

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,16 @@
-import { roboto, sora } from '../styles/fonts'
-import '../styles/globals.scss'
+import { roboto, sora } from "../styles/fonts";
+import "../styles/globals.scss";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "@/lib/apollo";
 
-import MainLayout from '@/components/Layout/MainLayout'
+import MainLayout from "@/components/Layout/MainLayout";
 
 export default function App({ Component, pageProps }) {
-  return ( 
-    <MainLayout> 
-       <Component {...pageProps} />
-    </MainLayout>
-  )
+  return (
+    <ApolloProvider client={client}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ApolloProvider>
+  );
 }
