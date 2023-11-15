c541a724225cbb4c256390a9f5cf7d47e3658665
Samuelfjr <samucaftww@gmail.com>
2023-11-14 21:59:50 -0300

    feat: Barra Progresso

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,6 +4,7 @@ import { ApolloProvider } from "@apollo/client";
 import { client } from "@/lib/apollo";
 
 import MainLayout from "@/components/Layout/MainLayout";
+import NextNProgress from "nextjs-progressbar";
 
 import { DefaultSeo } from "next-seo";
 import SEO from "../../next-seo-config";
@@ -12,6 +13,14 @@ export default function App({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
       <MainLayout>
+        <NextNProgress
+          color="#29D"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
+
         <DefaultSeo {...SEO} />
         <Component {...pageProps} />
       </MainLayout>
